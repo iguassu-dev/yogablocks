8b7fd2641c652c48ec0d9711816965b94991a01b
Iris <aiqi.li.dev@gmail.com>
2025-04-08 12:33:23 -0400

    added typography * created test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import {
   TypographyHeading1,
   TypographyHeading2,
+  TypographyHeading3,
+  TypographyBody,
+  TypographyLink,
+  TypographyCaption,
 } from "@/components/ui/typography";
 
 export default function LibraryPage() {
@@ -10,6 +14,17 @@ export default function LibraryPage() {
       <TypographyHeading2>
         This is your personal yoga knowledge base.
       </TypographyHeading2>
+      <TypographyHeading3>
+        Here you can store all your yoga knowledge, including poses, sequences,
+        and other information.
+      </TypographyHeading3>
+      <TypographyBody>
+        This is your personal yoga knowledge base.
+      </TypographyBody>
+      <TypographyLink href="/">Home</TypographyLink>
+      <TypographyCaption>
+        This is your personal yoga knowledge base.
+      </TypographyCaption>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,7 @@ const config: Config = {
     "./components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontSize: {
-        xs: ["0.75rem", { lineHeight: "1rem" }],
-        sm: ["0.875rem", { lineHeight: "1.25rem" }],
-        base: ["1rem", { lineHeight: "1.5rem" }],
-        lg: ["1.125rem", { lineHeight: "1.75rem" }],
-        xl: ["1.25rem", { lineHeight: "1.75rem" }],
-        "2xl": ["1.5rem", { lineHeight: "2rem" }],
-        "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-      },
-    },
+    extend: {},
     screens: {
       // Keep the defaults, but focus testing around 320-430px during Alpha
       xs: "375px",
